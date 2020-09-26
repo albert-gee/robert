a7763e1a89e02785123039a719838776923addcb
Albert <albert.gee.ca@gmail.com>
2020-09-25 20:52:21 -0700

    Creates tests for URLHelper class

---
+++ src/test/java/io/albert_gee/robort/helpers/URLHelperTest.java
@@ -0,0 +1,123 @@
+package io.albert_gee.robort.helpers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class URLHelperTest {
+
+    @Test
+    void makeUrlAbsoluteSuccess() {
+        String expectedResult = "http://www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteStartsWithOneSlashSuccess() {
+        String expectedResult = "http://www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("/index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteStartsWithTwoSlashesSuccess() {
+        String expectedResult = "http://www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("//www.site.com/index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteStartsWithHttpAndTwoSlashesSuccess() {
+        String expectedResult = "http://www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("http://www.site.com/index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteStartsWithHttpsAndTwoSlashesSuccess() {
+        String expectedResult = "https://www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("https://www.site.com/index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteStartsWithSchemaWithoutColonSuccess() {
+        String expectedResult = "http://www.site.com/https//www.site.com/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("https//www.site.com/index.php?param=1", "http://www.site.com");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteIncorrectFail() {
+        String expectedResult = "http://www.com.site/index.php?param=1";
+        String actualResult = URLHelper.makeUrlAbsolute("index.php?param=1", "http://www.site.com");
+        assertNotEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void makeUrlAbsoluteUrlIsNullFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute(null, "http://www.site.com");
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void makeUrlAbsoluteParentIsNullFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute("http://www.site.com", null);
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void makeUrlAbsoluteUrlAndParentAreNullFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute(null, null);
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void makeUrlAbsoluteUrlIsEmptyFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute(null, "http://www.site.com");
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void makeUrlAbsoluteParentIsEmptyFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute("http://www.site.com", null);
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void makeUrlAbsoluteUrlAndParentAreEmptyFail() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            URLHelper.makeUrlAbsolute(null, null);
+        });
+
+        String expectedMessage = "URL of host were not provided";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+}
